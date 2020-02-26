0d8204e079f2d53b7f844ee575237730646ce5ad
Matthew Russo <matthew@matthewclayrusso.com>
2020-02-25 23:17:36 -0800

    split files

---
+++ request.py
@@ -0,0 +1,64 @@
+# https://tools.ietf.org/html/rfc2616#section-5
+# Request       = Request-Line              ; Section 5.1
+#                 *(( general-header        ; Section 4.5
+#                  | request-header         ; Section 5.3
+#                  | entity-header ) CRLF)  ; Section 7.1
+#                 CRLF
+#                 [ message-body ]          ; Section 4.3
+class HttpRequest:
+
+    
+
+# 5.1 Request-Line
+# 
+#    The Request-Line begins with a method token, followed by the
+#    Request-URI and the protocol version, and ending with CRLF. The
+#    elements are separated by SP characters. No CR or LF is allowed
+#    except in the final CRLF sequence.
+# 
+#         Request-Line   = Method SP Request-URI SP HTTP-Version CRLF
+class HttpRequestLine:
+    
+
+# Sections 9.2 - 9.9
+class HttpMethod(Enum):
+    OPTIONS = auto()
+    GET     = auto()
+    HEAD    = auto()
+    POST    = auto()
+    PUT     = auto()
+    DELETE  = auto()
+    TRACE   = auto()
+    CONNECT = auto()
+
+class HttpRequestParser:
+
+    def parse():
+        print('unimplemented parse')
+
+    def _request_line():
+        print('unimplemented _request_line')
+
+    def _method():
+        print('unimplemented _method')
+
+    def _request_uri():
+        print('unimplemented _request_uri')
+
+    def _http_version():
+        print('unimplemented _http_version')
+
+    def _headers():
+        print('unimplemented _headers')
+
+    def _general_header():
+        print('unimplemented _general_header')
+
+    def _request_header():
+        print('unimplemented _request_header')
+
+    def _entity_header():
+        print('unimplemented _entity_header')
+
+    def _body():
+        print('unimplemented _body')
--- server.py
+++ server.py
@@ -1,86 +1,23 @@
 from enum import Enum
 import socket                
-  
-s = socket.socket()          
-print('Socket successfully created')
-  
-port = 12345                
-  
-s.bind(('', port))         
-print('socket binded to {}'.format(port))
-  
-# put the socket into listening mode with a backlog of 5 connections
-s.listen(5)
-print('socket is listening')
-  
-while True: 
-    conn, addr = s.accept()      
-    data = receiveAll(conn)
-    http_request = HttpRequest.parse(data)
-    handle(http_request)
-    conn.close()
 
-# https://tools.ietf.org/html/rfc2616#section-5
-# Request       = Request-Line              ; Section 5.1
-#                 *(( general-header        ; Section 4.5
-#                  | request-header         ; Section 5.3
-#                  | entity-header ) CRLF)  ; Section 7.1
-#                 CRLF
-#                 [ message-body ]          ; Section 4.3
-class HttpRequest:
+class HttpServer:
+    @staticmethod
+    def on_port(port: int) -> Self:
+        new_server = HttpServer()
+        new_server.socket = socket.socket()          
+        new_server.socket.bind(('', port))         
+        # put the socket into listening mode with a backlog of 5 connections
+        new_server.socket.listen(5)
 
-    
+    def serve():
+        while True: 
+            conn, addr = s.accept()
+            http_request = self.parse_request(conn)
+            handle(http_request)
+            conn.close()
 
-# 5.1 Request-Line
-# 
-#    The Request-Line begins with a method token, followed by the
-#    Request-URI and the protocol version, and ending with CRLF. The
-#    elements are separated by SP characters. No CR or LF is allowed
-#    except in the final CRLF sequence.
-# 
-#         Request-Line   = Method SP Request-URI SP HTTP-Version CRLF
-class HttpRequestLine:
-    
+    def parse_request(conn: Connection) -> HttpRequest:
+        data = receive_all(conn)
+        http_request = HttpRequest.parse(data)
 
-# Sections 9.2 - 9.9
-class HttpMethod(Enum):
-    OPTIONS = auto()
-    GET     = auto()
-    HEAD    = auto()
-    POST    = auto()
-    PUT     = auto()
-    DELETE  = auto()
-    TRACE   = auto()
-    CONNECT = auto()
-
-class HttpRequestParser:
-
-    def parse():
-        print('unimplemented parse')
-
-    def _request_line():
-        print('unimplemented _request_line')
-
-    def _method():
-        print('unimplemented _method')
-
-    def _request_uri():
-        print('unimplemented _request_uri')
-
-    def _http_version():
-        print('unimplemented _http_version')
-
-    def _headers():
-        print('unimplemented _headers')
-
-    def _general_header():
-        print('unimplemented _general_header')
-
-    def _request_header():
-        print('unimplemented _request_header')
-
-    def _entity_header():
-        print('unimplemented _entity_header')
-
-    def _body():
-        print('unimplemented _body')
